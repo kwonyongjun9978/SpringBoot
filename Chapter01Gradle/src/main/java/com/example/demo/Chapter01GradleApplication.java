12fdb053305064e1e827523b173695a913683753
Jun <rnsuz74@naver.com>
2023-05-02 23:49:09 +0900

    Gradle

---
+++ Chapter01Gradle/src/main/java/com/example/demo/Chapter01GradleApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter01GradleApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(Chapter01GradleApplication.class, args);
+		
+		SpringApplication springApplication = new SpringApplication(Chapter01GradleApplication.class);
+		springApplication.setWebApplicationType(WebApplicationType.NONE);
+		springApplication.run(args);
+	}
+
+}
